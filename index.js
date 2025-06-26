ceec5b90ef18fe7af035da65b1640a5272bbc600
GuillaumeLeBerreBIT <leberreguillaume.glb@gmail.com>
2025-06-26 23:08:04 +0200

    Retrieve the weather data

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
+const baseUrl = "https://api.weatherapi.com/v1"
+const apiKey = "eae56411965046928cc212140252506"
 
-document.addEventListener('click', (event) => {
-    let query = event.target.value;
+const inputField = document.querySelector('#query');
 
-    // Parse into an API to retrieve the data from City
+inputField.addEventListener('keypress', (event) => {
+    
+    if (event.key == "Enter") {
+
+        // console.log(inputField.value);
+        let data = fetchWeatherData(inputField.value);
+    };
 });
+
+async function fetchWeatherData (q) {
+    
+    try{
+        let queryUrl = `${baseUrl}/current.json?q=${q}&key=${apiKey}`;
+        let response = await fetch(queryUrl);
+        
+        if (!response.ok) {
+            throw new Error('Unable to retrieve the data for: ' + q);
+        }
+
+        let data = await response.json(); // Need to await the response to get the data from the Promises returned from Fetch. 
+        // console.log(await response.json());
+
+        return data
+
+        return 
+    } catch (err) {
+        console.log(err);
+    }
+}
