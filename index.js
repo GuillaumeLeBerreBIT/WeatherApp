714b49f9e856fd5a8fa9d8f0e48bd4d30c875f68
GuillaumeLeBerreBIT <leberreguillaume.glb@gmail.com>
2025-06-27 22:54:30 +0200

    Update search bar style

--- index.js
+++ index.js
@@ -1,53 +1,94 @@
-const baseUrl = "https://api.weatherapi.com/v1"
-const apiKey = "eae56411965046928cc212140252506"
+const baseUrl = "https://api.weatherapi.com/v1";
+const apiKey = "eae56411965046928cc212140252506";
 
-const inputField = document.querySelector('#query');
+const inputField = document.querySelector("#query");
 
-inputField.addEventListener('keypress', (event) => {
-    
-    if (event.key == "Enter") {
-
-        // console.log(inputField.value);
-        let data = fetchWeatherData(inputField.value);
-
-
-    };
+inputField.addEventListener("keypress", (event) => {
+  if (event.key == "Enter") {
+    // console.log(inputField.value);
+    let data = fetchWeatherData(inputField.value);
+  }
 });
 
-function fetchWeather () {
-    fetchWeatherData(inputField.value);
+function fetchWeather() {
+  fetchWeatherData(inputField.value);
 }
 
-async function fetchWeatherData (q) {
-    
-    try{
-        let queryUrl = `${baseUrl}/current.json?q=${q}&key=${apiKey}`;
-        let response = await fetch(queryUrl);
-        
-        if (!response.ok) {
-            throw new Error('Unable to retrieve the data for: ' + q);
-        }
+async function fetchWeatherData(q) {
+  try {
+    let queryUrl = `${baseUrl}/current.json?q=${q}&key=${apiKey}`;
+    let response = await fetch(queryUrl);
 
-        // let data = await response.json(); // Need to await the response to get the data from the Promises returned from Fetch. 
-        // console.log(await response.json());
-
-        await response.json().then(data => {
-            const element = document.querySelector('.weather-info');
-            const newDiv = document.createElement('div');
-            console.log(data);
-            newDiv.innerHTML = `
-            <h2>${data.location.name}</h2>
-            <h3>${data.location.country}</h3>
-            <h1>${data.current.temp_c}</h1>
-
-            <div>${data.current.condition.text}</div>
-
-            `;
-            element.appendChild(newDiv);
-        })
-
-
-    } catch (err) {
-        console.log(err);
+    if (!response.ok) {
+      throw new Error("Unable to retrieve the data for: " + q);
     }
+
+    let data = await response.json();
+    cardWeather = new WeatherCardGenerator(data);
+    cardWeather.generateCard();
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+class WeatherCardGenerator {
+  constructor(weatherData) {
+    this.name = weatherData.location.name;
+    this.temp = weatherData.current.temp_c;
+    this.image = weatherData.current.condition.icon;
+    this.country = weatherData.location.country;
+    this.condition = weatherData.current.condition.text;
+    this.wind = weatherData.current.wind_kph;
+    this.wind_dir = weatherData.current.wind_dir;
+    this.uv = weatherData.current.uv;
+    this.pressure = weatherData.current.pressure_mb;
+    this.precip = weatherData.current.precip_mm;
+  }
+
+  generateCard() {
+    const element = document.querySelector(".weather-info");
+    element.innerHTML = "";
+
+    const newDiv = document.createElement("div");
+    newDiv.innerHTML = this.generateCardTemplate();
+    element.appendChild(newDiv);
+  }
+
+  generateCardTemplate() {
+    return `<div class="weather-card roboto-font justify-left">
+            <h2>${this.name}</h2>
+            <div class="justify-temp-icon">
+                <h1>${this.temp}°C</h1>
+                <img src="https:${this.image.replace(
+                  "64x64",
+                  "128x128"
+                )}" />
+            </div>
+            <h3>${this.country}</h3>
+            <h4 class="cs">${this.condition}</h4>
+            <div class="grid-info"> 
+                <div class="item-info">
+                    <span><i class="fa-solid fa-wind"></i></span>
+                    <span>${this.wind} Km/H</span>
+                    <span><i class="fa-solid fa-location-arrow"></i> ${
+                      this.wind_dir
+                    }</span>
+                </div>
+                <div class="item-info">
+                    <span><i class="fa-solid fa-sun"></i></span>
+                    <span>${this.uv}</span>
+                    <span>UV Index</span>
+                </div>
+                <div class="item-info">
+                    <span><i class="fas fa-thermometer-half"></i></span>
+                    <span>${this.pressure} kPa</span>
+                </div>
+                <div class="item-info">
+                    <span><i class="fa-solid fa-cloud-showers-heavy"></i></span>
+                    <span>${this.precip} mm</span>
+                </div>
+            </div>
+        </div>
+        `;
+  }
 }
